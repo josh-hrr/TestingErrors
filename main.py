b29d7b4909d11c99da388ef7395a31fb406476ce
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-12-27 03:04:44 +0000

    update

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ except ZeroDivisionError:
   print("Division by zero is not allowed")
 
 print("code after exception execution")
+
+#reading a file
+
+myFile = open("myfile.txt", "w")  
+print("start typing... ") 
+writeThis = "" 
+while writeThis != "#":
+  writeThis = input("enter an input: ")
+  myFile.write(writeThis + "\n") 
+myFile.close()
+
+myFile = open("myfile.txt", "r")  
+print(myFile.read()) 
+myFile.close()
